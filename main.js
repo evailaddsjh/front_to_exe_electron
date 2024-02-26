1e327a25d55e3b4fa12a5bb8b8f39a2c89ad0c5a
shijiahao <1354642027@qq.com>
2024-02-26 10:08:38 +0800

    Enable auto hide menu bar in full screen mode

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow() {
     width: 800,
     height: 600,
     fullscreen:true,
-    // autoHideMenuBar: true,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     }
